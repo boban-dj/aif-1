5cc06807db2fa356a33c9f10821115dc6ee5d5ce
bobandj71 <bobandj71@gmail.com>
2017-04-11 18:18:30 +0200

    rm

--- BK_petya/Scripts/kill_chrome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for i in $(ps x | grep chrome | cut -d"?" -f1 | grep -v chrome); do kill -9 $i ; done
-rm -R /home/boban/.cache/google-chrome/*
--- BK_petya/Scripts/kill_whatsapp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in $(ps x | grep Whatsapp | cut -d"?" -f1 | grep -v Whatsapp); do kill -9 $i ; done
